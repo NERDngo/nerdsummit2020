4d81125f8e8ceac4fc7050975adba61495ec99d6
Clayton Salem <claytonjsalem@gmail.com>
2019-09-19 11:02:47 -0400

    Add the index file and updated migrations

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser')
+const knex = require('./knex/knex.js');
+
+app.set("port", process.env.PORT || port);
+
+app.disable('x-powered-by');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+
+    knex.from('session_blobs')
+        .select('session_blobs')
+        .orderBy('id', 'desc')
+        .first()
+        .then(data => {
+            console.log(data);
+            res.setHeader('Content-Type', 'text/plain')
+            res.write('you get:\n')
+            res.end(JSON.stringify(data, null, 2))
+        })
+
+})
+
+app.post('/', (req, res) => {
+
+    const secret = req.header("X-Secret");
+
+    if (!secret || secret !== "somelongrandomstring") {
+        return res.send("406");
+    }
+
+
+    knex('session_blobs').insert({ session_blobs: req.body }).then(data => {
+        res.setHeader('Content-Type', 'text/plain')
+        res.write('you saved:\n')
+        res.end(JSON.stringify(data, null, 2))
+    });
+
+
+});
+
+app.listen(app.get("port"), () => console.log(`Example app listening on port ${port}!`))
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
+      database: 'postgres://pqnlifhqwzfnfz:04b1a10579b050c962e489fad752f31ece48373afaf4ede8276ec47914d03853@ec2-54-235-100-99.compute-1.amazonaws.com:5432/ddndkht8tt6gkb',
       user: 'username',
       password: 'password'
     },
@@ -51,7 +51,8 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: __dirname + '/knex/migrations',
     }
   }
 

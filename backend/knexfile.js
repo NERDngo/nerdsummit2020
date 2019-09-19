6f94ecb73b91b8403c29da2427c436410fcc78cb
Clayton Salem <claytonjsalem@gmail.com>
2019-09-19 11:05:01 -0400

    Use db string directly

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -41,11 +41,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'postgres://pqnlifhqwzfnfz:04b1a10579b050c962e489fad752f31ece48373afaf4ede8276ec47914d03853@ec2-54-235-100-99.compute-1.amazonaws.com:5432/ddndkht8tt6gkb',
-      user: 'username',
-      password: 'password'
-    },
+    connection: 'postgres://pqnlifhqwzfnfz:04b1a10579b050c962e489fad752f31ece48373afaf4ede8276ec47914d03853@ec2-54-235-100-99.compute-1.amazonaws.com:5432/ddndkht8tt6gkb',
     pool: {
       min: 2,
       max: 10

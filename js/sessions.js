01bff634fdb0051d34193e9dd9fe432347654952
Rick Hood <richard.hood@commonmediainc.com>
2019-10-28 19:24:20 -0400

    added new js/sessions.js from Clayton

--- js/sessions.js
+++ js/sessions.js
@@ -1,10 +1,20 @@
-// see https://stackoverflow.com/a/34908037/5855010
 
+// Call to function with anonymous callback
+loadJSON(function (response) {
+    const rows = massageData(JSON.parse(response).feed.entry)
+    const saturday = rows.filter(row => row.day === "Saturday");
+    const sunday = rows.filter(row => row.day === "Sunday");
+    createSessionList("Saturday", saturday);
+    createSessionList("Sunday", sunday);
+
+});
+
+// see https://stackoverflow.com/a/34908037/5855010
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../data/sessions.json', true);
-    xobj.onreadystatechange = function() {
+    xobj.open('GET', 'https://spreadsheets.google.com/feeds/cells/17EDt6Pu6xefcwT2C1UsYB7m0Ek-Vb1Us8Azfn3a_eso/1/public/full?alt=json', true);
+    xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // .open will NOT return a value but simply returns undefined in async mode so use a callback
             callback(xobj.responseText);
@@ -13,45 +23,48 @@ function loadJSON(callback) {
     xobj.send(null);
 }
 
-// Call to function with anonymous callback
-loadJSON(function(response) {
-    data = JSON.parse(response);
-    // loop over the data object
-    for (var key in data) {
-        for (var i = 0; i < data[key].length; i++) {
-            var id = data[key][i].id;
-            var order = data[key][i].order;
-            var name = data[key][i].name;
-            var start = data[key][i].start;
-            var end = data[key][i].end;
-            var speaker = data[key][i].speaker;
-            var type = data[key][i].type;
-            var room = data[key][i].room;
-            //var tags = data[key][i].tags;
-            var description = data[key][i].description;
-            var session = document.createElement('div');
-            session.className = 'session';
-            session.innerHTML =
-                '<div class="top" id="'+ id + '">' +
-                '<div class="time">' + start + ' - ' + end + '</div>' +
-                '<div class="room">Room: ' +room + '</div>' +
-                '<div class="title">' + '<h4 class="name">' + name + '</h4>' + '</div>' +
-                '<h5 class="speaker">' + speaker + '</h5>' +
-                '<p class="tags">Track: ' + type + '</p>' +
-                '</div>' +
-                '<div class="info">' +
-                '<p>' + description + '</p>' +
-                '</div>';
-            // index.html has this:  <div id="sessions"></div>
-            document.getElementById(key).appendChild(session);
-           // $(key).html(session);
-        }
-    }
-});
+function massageData(array) {
+    const keys = array
+        .filter(element => element.gs$cell.row === "1")
+        .map(element => ({ keyName: element.content.$t, columnNumber: element.gs$cell.col }));
+
+    const rows = array
+        .filter(element => element.gs$cell.row !== "1")
+        .reduce((acc, curr) => {
+            if (typeof acc[curr.gs$cell.row] === 'undefined') {
+                acc[curr.gs$cell.row] = {};
+            }
+            const key = keys.filter(el => el.columnNumber === curr.gs$cell.col).map(el => el.keyName);
+            acc[curr.gs$cell.row][key] = curr.content.$t
+            return acc
+        }, [])
+        .filter(row => row); // because we're using index numbers to fill the array, and we start at 2, the empty elements need to be filtered.
+
+    return rows;
+}
+
+function createSessionList(dayId, sessions) {
+    const dayElement = document.querySelector(`#${dayId}`);
+    console.log(dayElement)
+    sessions.forEach(session => {
+        let sessionElement = document.createElement('div');
+        sessionElement.className = 'session';
+        sessionElement.innerHTML =
+            '<div class="top" id="' + session.id + '">' +
+            '<div class="time">' + session.start + ' - ' + session.end + '</div>' +
+            '<div class="room">Room: ' + session.room + '</div>' +
+            '<div class="title">' + '<h4 class="name">' + session.name + '</h4>' + '</div>' +
+            '<h5 class="speaker">' + session.speaker + '</h5>' +
+            '<p class="tags">Track: ' + session.type + '</p>' +
+            // '<a href="' + session.speakerLink + '">' + session.speaker + '</a>' +
+            // '<a href="' + session.slideLink + '">' + session.name + '</a>'
+            '</div>' +
+            '<div class="info">' +
+            '<p>' + session.description + '</p>' +
+            '</div>';
+        dayElement.appendChild(sessionElement);
+    })
 
 
-
-
-
-
+}
 
